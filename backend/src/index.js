e5cd328d44cfe079d7ed6d82e9aa148e0007f69f
Abhishek09yadav <monuy8830@gmail.com>
2025-03-05 21:44:21 +0530

    initialize frontend and backend with React, Vite, and Express setup

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import connectDB from "./lib/db.js";
+const PORT = process.env.PORT || 5999;
+const app = express();
+dotenv.config();
+
+app.listen(PORT, () => {
+  console.log(`server is running on port number ${PORT}`);
+  connectDB();
+});
+app.get("/", (req, res) => res.send(`server is running on port ${PORT}`));
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
