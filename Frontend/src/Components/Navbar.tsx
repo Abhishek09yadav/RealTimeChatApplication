c1dcc5753e2eb9f1d417e668b29b8305ff4379dc
Abhishek09yadav <monuy8830@gmail.com>
2025-03-26 00:43:27 +0530

    Refactor Navbar component; enhance layout and user interaction; integrate logout functionality and user profile display

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,13 +1,59 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import { useAuthStore } from "@/store/useAuthStore";
-
-const Navbar = () => {
-      const { authUser } = useAuthStore();
-  
-  return (
-    <div>Navbar</div>
-  )
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { useRouter } from "next/navigation";
+interface AuthStore {
+  logout: () => void;
+  authUser: object;
 }
+const Navbar = () => {
+  const { logout, authUser } = useAuthStore() as AuthStore;
+  const router = useRouter();
+  return (
+    <header
+      className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+    backdrop-blur-lg bg-base-100/80"
+    >
+      <div className="h-16 container mx-auto px-4">
+        <div className="flex items-center h-full gap-4 justify-between ">
+          {/* logo */}
+          <div
+            className="flex items-center gap-3 cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            <div className="h-10 w-10 rounded-lg bg-primary/10 flex items-center justify-center ">
+              <MessageSquare />
+            </div>
+            <h1>Chat App</h1>
+          </div>
+          {/* settings and profile section */}
+          <div className="flex flex-row items-center gap-3 cursor-pointer">
+            <div className="flex items-center gap-3">
+              <div
+                className=" btn btn-sm gap-2 transition-colors"
+                onClick={() => router.push("/Settings")}
+              >
+                <Settings className="w-4 h-4" />
+                <span>Settings</span>
+              </div>
+            </div>
+            {authUser && (
+              <>
+                <div className="flex items-center gap-3 cursor-pointer">
+                  <User />
+                  <span>{authUser?.fullName || "profile"}</span>
+                </div>
+                <button onClick={logout} className="cursor-pointer">
+                  <LogOut />
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
