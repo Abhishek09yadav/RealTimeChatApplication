ca1320dadf0659a5c073fce8b21ebaee6e94f4ca
Abhishek09yadav <monuy8830@gmail.com>
2025-05-02 00:20:32 +0530

    Refactor SidebarSkeleton component: update structure and add skeleton contacts array

--- Frontend/src/Components/skeletons/SidebarSkeleton.tsx
+++ Frontend/src/Components/skeletons/SidebarSkeleton.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Users } from "lucide-react";
 
 const SidebarSkeleton = () => {
+  const sekeletonContacts = Array(8).fill(null)
   return (
-    <div>
+    <aside className="h-full w-20 ">
       
-    </div>
+    </aside>
   )
 }
 
--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import HomePage from "@/Components/HomePage";
+// import Navbar from "@/Components/Navbar";
 
 export default function Home() {
 
   return <>
-  
+  {/* <Navbar/> */}
   <HomePage/>
 
   </>;
