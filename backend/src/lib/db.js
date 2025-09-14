b4cd57aa78479eb5993b954e746f996f1d2f1945
Abhishek09yadav <monuy8830@gmail.com>
2025-09-14 16:40:20 +0530

    adding monorepo

--- backend/src/lib/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const connectDB = async() =>{
-try{ 
-  const conn = await mongoose.connect(process.env.MONGODB_URI);
-  console.log( `MongoDB connected: ${conn.connection.host}` );
-}
-catch{e =>{console.log('connection error',e); }}
-}
-export default connectDB; 
